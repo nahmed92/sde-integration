76659587580e03e471bd6429b0ce11f3e9e2ff91
Wajih ur Rahman <wurahman@etilizepak.com>
2015-10-19 19:12:35 +0500

    SDE-2286 SDE is adding new set to attributes which are not repeatable
    - Fixes adding to non-repeatable issue
    
    - Displays accept set button only when parameter is repeatable

--- web/app/app_components/inference/inferenceController.js
+++ web/app/app_components/inference/inferenceController.js
@@ -188,7 +188,7 @@ app.controller('InferenceController', function($scope, inferenceService, _, $win
     var relatedParameters = [];
     angular.forEach(list, function(inference) {
       // If the inference does not have any related parameter ids, we can accept id right away, otherwise we will call the acceptInferenceSet() method, but only once per set
-      if (angular.isUndefined(inference.setNo)) {
+      if (inference.isRepeatable === false || angular.isUndefined(inference.setNo)) {
         $scope.acceptInference(inference);
       } else {
         relatedParameters.push(inference);
